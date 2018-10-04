fe28e0d93c4d5def249817f14a72c6ebc234c39b
d.schilling <d.schilling@actbig.de>
2018-10-04 13:49:37 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ CSS_E2FI1_SAE_2018_19/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSS_E2FI1_SAE_2018_19
+{
+    class Program
+    {
+        static void Main(string[] args) {
+        }
+    }
+}
